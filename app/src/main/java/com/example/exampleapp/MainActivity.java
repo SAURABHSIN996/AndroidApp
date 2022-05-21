4290ce036c2f3b2219c4085c6ddf3eef559013db
saurabh singh <saurabh.9901s@gmail.com>
2022-05-21 15:16:35 +0530

    updated button

--- app/src/main/java/com/example/exampleapp/MainActivity.java
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.exampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickFunction(View view){
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Log.i("info","it worked!");
+        Log.i("values", nameEditText.getText().toString());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
